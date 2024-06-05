7a0680fa08e8765027fdbbfa60e18bda12937875
Faisal Aslam <faisalaslam218@gmail.com>
2024-06-06 02:44:56 +0500

    3.0.0

--- lib/Repository/news_repo.dart
+++ lib/Repository/news_repo.dart
@@ -8,20 +8,16 @@ import 'package:news_app/Model/headlines_model.dart';
 
 class NewsRepository {
   Future<NewsHeadlinesModel> fetchNewsHeadlines() async {
-    // api url which i used 'newsapi.org' website
     String url =
         'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=06e8f5b5e7094e2e9f0dd38fb5d300f5';
 
-    // http package to get the data
     final response = await http.get(Uri.parse(url));
     if (kDebugMode) {
       print(response.body);
     }
     if (response.statusCode == 200) {
-      // for return body
       final body = jsonDecode(response.body);
       return NewsHeadlinesModel.fromJson(body);
-      // it will return complete body of data
     }
     throw Exception('Error');
   }
